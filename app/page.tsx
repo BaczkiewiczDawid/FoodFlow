7e655b806ff3b5068d274d61f34698777000249a
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2024-11-27 22:31:47 +0100

    feat: home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,20 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import {hasEnvVars} from "@/utils/supabase/check-env-vars";
+import Image from "next/image"
+import HeroImage from "@/app/assets/images/undraw_breakfast_psiw.svg"
+import {Button} from "@/components/ui/button";
 
 export default async function Index() {
     return (
         <>
-            {hasEnvVars ? <SignUpUserSteps/> : <ConnectSupabaseSteps/>}
+            <div
+                className={"flex flex-col justify-center items-center text-center max-w-[90%] lg:max-w-[60%] xl:max-w-[40%] 2xl:max-w-[45%]"}>
+                <h1 className={"text-2xl font-bold"}>Organize your meals and shopping online!</h1>
+                <p className={"mt-4"}>Start for free now!</p>
+                <Image className={"w-3/4 mt-12"} src={HeroImage} alt={"Hero image"}/>
+                <div className={"mt-12 flex justify-center items-center flex-col"}>
+                    <Button className={"px-8"}>Join now!</Button>
+                    <Button variant={"link"} className={"mt-2"}>Already have an account? Login now</Button>
+                </div>
+            </div>
         </>
     );
 }
