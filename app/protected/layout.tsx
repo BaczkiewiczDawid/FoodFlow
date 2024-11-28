5a2ea4a71defb1dfa47ce5ae49f5b9b8ca4a0086
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2024-11-28 22:30:58 +0100

    Versioning untracked files

--- app/protected/layout.tsx
+++ app/protected/layout.tsx
@@ -1,6 +1,6 @@
 export default function RootLayout({children}: { children: React.ReactNode }) {
     return (
-        <div className={"h-screen w-screen p-4"}>
+        <div className={"h-screen w-screen py-4 px-8"}>
             {children}
         </div>
     );
--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,7 +1,5 @@
 import {createClient} from "@/utils/supabase/server";
 import {redirect} from "next/navigation";
-
-import {Chart} from "@/components/chart-components/chart";
 import {MacroChart} from "@/components/chart-components/macro-chart";
 import {MealCard} from "@/components/meal-card";
 
@@ -74,7 +72,7 @@ export default async function ProtectedPage() {
                 <h1 className={"font-normal text-2xl lg:text-xl"}>Hello, <p className={"font-bold"}>{user.email}</p>
                 </h1>
                 <p className={"mt-4 lg:mt-12"}>Today, You've eaten</p>
-                <div className={"grid grid-cols-2 gap-x-4 gap-y-8 w-[90%] lg:grid-cols-4 lg:mt-4"}>
+                <div className={"grid grid-cols-2 gap-x-4 gap-y-8 w-[90%] lg:grid-cols-4 mt-4"}>
                     <MacroChart data={data} name={"Kcal"}/>
                     <MacroChart data={data} name={"Carbs"}/>
                     <MacroChart data={data} name={"Protein"}/>
