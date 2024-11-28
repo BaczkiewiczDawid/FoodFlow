c1da9b088611c25c9da63c81efa17b52d185ea63
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2024-11-28 22:04:36 +0100

    feat: home page

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -3,6 +3,7 @@ import {redirect} from "next/navigation";
 
 import {Chart} from "@/components/chart-components/chart";
 import {MacroChart} from "@/components/chart-components/macro-chart";
+import {MealCard} from "@/components/meal-card";
 
 export default async function ProtectedPage() {
     const supabase = await createClient();
@@ -31,6 +32,41 @@ export default async function ProtectedPage() {
         ]
     }
 
+    const meals = [
+        {
+            id: 1,
+            name: "Chicken Cheese pasta",
+            ingredients: ["Chicken", "Cheese", "Pasta", "Tomato souce"],
+            macros: {
+                kcal: 530,
+                protein: 52,
+                carbs: 40,
+                fat: 10
+            }
+        },
+        {
+            id: 2,
+            name: "Chicken Cheese pasta",
+            ingredients: ["Chicken", "Cheese", "Pasta", "Tomato souce"],
+            macros: {
+                kcal: 530,
+                protein: 52,
+                carbs: 40,
+                fat: 10
+            }
+        },
+        {
+            id: 3,
+            name: "Chicken Cheese pasta",
+            ingredients: ["Chicken", "Cheese", "Pasta", "Tomato souce"],
+            macros: {
+                kcal: 530,
+                protein: 52,
+                carbs: 40,
+                fat: 10
+            }
+        }
+    ]
 
     return (
         <div className="flex-1 w-full flex flex-col gap-12">
@@ -40,6 +76,16 @@ export default async function ProtectedPage() {
                 <MacroChart data={data} name={"Carbs"}/>
                 <MacroChart data={data} name={"Protein"}/>
                 <MacroChart data={data} name={"Fat"}/>
+            </div>
+            <h2 className={"font-bold text-xl"}>Your next meal</h2>
+            <div className={"flex flex-col gap-y-4"}>
+                {meals.map((meal) => {
+                    return (
+                        <MealCard id={meal.id} name={meal.name} ingredients={meal.ingredients} macros={meal.macros}/>
+                    )
+                })}
+                <div className={"w-full bg-gray-700 rounded"}></div>
+                <div className={"w-full bg-gray-700 rounded"}></div>
             </div>
         </div>
     );
