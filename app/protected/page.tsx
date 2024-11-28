e978b87eda060387e79d687ffafaa713c707d3f9
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2024-11-28 20:34:47 +0100

    feat: home page

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -1,6 +1,8 @@
 import {createClient} from "@/utils/supabase/server";
-import {InfoIcon} from "lucide-react";
 import {redirect} from "next/navigation";
+
+import {Chart} from "@/components/chart-components/chart";
+import {MacroChart} from "@/components/chart-components/macro-chart";
 
 export default async function ProtectedPage() {
     const supabase = await createClient();
@@ -13,8 +15,32 @@ export default async function ProtectedPage() {
         return redirect("/sign-in");
     }
 
+
+    const data = {
+        labels: ["Calories, Total"],
+        datasets: [
+            {
+                label: "Calories",
+                data: [320, 120],
+                backgroundColor: [
+                    'rgb(255, 99, 132)', "rgb(225,225,225)"
+                ],
+                borderWidth: 0,
+                weight: 2,
+            },
+        ]
+    }
+
+
     return (
         <div className="flex-1 w-full flex flex-col gap-12">
+            <h1 className={"font-normal"}>Hello, <p className={"font-bold"}>{user.email}</p></h1>
+            <div className={"grid grid-cols-2 gap-x-4 gap-y-8 w-[90%]"}>
+                <MacroChart data={data} name={"Kcal"}/>
+                <MacroChart data={data} name={"Carbs"}/>
+                <MacroChart data={data} name={"Protein"}/>
+                <MacroChart data={data} name={"Fat"}/>
+            </div>
         </div>
     );
 }
