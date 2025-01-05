df30056024c016df57912a00535a9b59070defe7
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2025-01-05 18:00:28 +0100

    feat: display summary

--- app/protected/calendar/page.tsx
+++ app/protected/calendar/page.tsx
@@ -1,6 +1,8 @@
 import {NewMeal} from "@/components/new-meal";
 import {MealsList} from "@/components/calendar/meals-list";
 import {getMealsForDay} from "@/app/protected/calendar/actions";
+import moment from "moment";
+import {getDates} from "@/hooks/get-dates";
 
 const mealOptions = [
     {
@@ -22,12 +24,33 @@ const mealOptions = [
 ]
 
 export default async function Page() {
-    const meals = await getMealsForDay("2025-05-01", "a6801067-87a6-406b-a73a-94e26e89f9b7")
+    const {today, dateString} = getDates()
+
+    const meals = await getMealsForDay(today, "a6801067-87a6-406b-a73a-94e26e89f9b7")
+
+    const totalMacro = meals.map((meal) => {
+        return meal.macro.reduce((acc, ingredient) => {
+            acc.kcal += ingredient.kcal
+            acc.protein += ingredient.protein
+            acc.fat += ingredient.fat
+            acc.carbs += ingredient.carbs
+
+            return acc
+        }, {kcal: 0, protein: 0, fat: 0, carbs: 0})
+    })
 
     return (
         <div>
             <NewMeal mealOptions={mealOptions}/>
-            <h1 className={"font-bold text-xl"}>31 December, 2024</h1>
+            <h1 className={"font-bold text-xl"}>{dateString}</h1>
+            <div>
+                <div className={"flex justify-between font-light text-sm md:w-1/3"}>
+                    <p>{totalMacro[0].kcal} Kcal</p>
+                    <p>{totalMacro[0].protein} B</p>
+                    <p>{totalMacro[0].fat} T</p>
+                    <p>{totalMacro[0].carbs} W</p>
+                </div>
+            </div>
             <div className={"flex flex-col gap-y-4 mt-8 lg:w-2/3 xl:w-1/3"}>
                 {mealOptions.map((option, index) => {
                     const mealData = meals.filter((meal) => meal.mealType === option.value)
