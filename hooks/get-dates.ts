31a6ceb13d3dc2bc28a4c62578382d2f948f6490
BaczkiewiczDawid <baczkiewicz.dawid22@gmail.com>
2025-01-26 09:42:08 +0100

    fix: dashboard date format

--- hooks/get-dates.ts
+++ hooks/get-dates.ts
@@ -1,7 +1,7 @@
 import moment from "moment";
 
 export const getDates = () => {
-    const today = moment().format("YYYY-DD-MM")
+    const today = moment().format("YYYY-MM-DD")
     const todayFormatted = moment().format("YYYY-MM-DD")
     const dateString = moment().format("DD MMMM, YYYY")
 
